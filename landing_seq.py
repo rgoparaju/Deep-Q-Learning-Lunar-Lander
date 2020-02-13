5a6ed4d6ee656271d858b454c3ef6102b0eac90a
rgoparaju <tejagoparaju@gmail.com>
2020-02-13 12:09:24 -0600

    Update landing_seq.py

--- landing_seq.py
+++ landing_seq.py
@@ -58,7 +58,8 @@ class NeuralNetwork(nn.Module):
         # of the environment. 
         return q_values
 
-# Experience Replay
+# Experience Replay - allows the AI to learn from the previous rewards it has received to determine how 
+# it should proceed with learning. 
 
 class ExperienceReplay(object):
     
@@ -81,56 +82,11 @@ class Landing_Sequence():
     def __init__(self, input_size, nb_action, gamma):
         self.gamma = gamma
         self.reward_window = []
-        self.model = Network(input_size, nb_action)
+        self.model = NeuralNetwork(input_size, nb_action)
         self.memory = ExperienceReplay(100000)
         self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
         self.last_state = torch.Tensor(input_size).unsqueeze(0)
         self.last_action = 0
         self.last_reward = 0
     
-    def select_action(self, state):
-        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
-        action = probs.multinomial(1)
-        return action.data[0,0]
-    
-    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
-        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
-        next_outputs = self.model(batch_next_state).detach().max(1)[0]
-        target = self.gamma*next_outputs + batch_reward
-        td_loss = F.smooth_l1_loss(outputs, target)
-        self.optimizer.zero_grad()
-        td_loss.backward(retain_graph = True)
-        self.optimizer.step()
-    
-    def update(self, reward, new_signal):
-        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
-        self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
-        action = self.select_action(new_state)
-        if len(self.memory.memory) > 100:
-            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
-            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
-        self.last_action = action
-        self.last_state = new_state
-        self.last_reward = reward
-        self.reward_window.append(reward)
-        if len(self.reward_window) > 1000:
-            del self.reward_window[0]
-        return action
-    
-    def score(self):
-        return sum(self.reward_window)/(len(self.reward_window)+1.)
-    
-    def save(self):
-        torch.save({'state_dict': self.model.state_dict(),
-                    'optimizer' : self.optimizer.state_dict(),
-                   }, 'last_landing_seq.pth')
-    
-#    def load(self):
-#        if os.path.isfile('last_landing_seq.pth'):
-#            print("Loading...")
-#            checkpoint = torch.load('last_landing_seq.pth')
-#            self.model.load_state_dict(checkpoint['state_dict'])
-#            self.optimizer.load_state_dict(checkpoint['optimizer'])
-#            print("Loaded")
-#        else:
-#            print("No Sequence Found")
+"""    
